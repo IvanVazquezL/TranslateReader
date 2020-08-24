d502369d56b0dbe2e67492f0011b247f51ab6ae1
IvanVazquezL <ivanvl_11@hotmail.com>
2020-08-23 21:56:57 -0500

    test book and exercises

--- javascript/app3.js
+++ javascript/app3.js
@@ -1,5 +1,43 @@
 var pageDedicace = document.querySelector('.pageDedicace');
 var audioDedicace = document.querySelector('.audioContainer');
+var audioChapitreOne = document.querySelector('.audioContainerOne');
+var pageChapitreOne = document.querySelector('#chapitreOne');
+var audioChapitreTwo = document.querySelector('.audioContainerTwo');
+var pageChapitreTwo = document.querySelector('#chapitre2');
+var words = document.querySelectorAll(".tooltips");
+
+for (var i = 0; i < words.length; i++) {
+  words[i].addEventListener("click", function(){
+    console.log(this.firstChild.textContent);
+  });
+}
+// $('span' ).tooltip({
+// show: false,
+// hide: false
+// });
+
+var tooltips = document.querySelectorAll('.tooltips span');
+var tooltipsUp = document.querySelectorAll('.tooltipsUp em');
+
+window.onmousemove = function (e) {
+    var x = (e.clientX + 10) + 'px',
+        y = (e.clientY + 20) + 'px',
+        yp = (e.clientY - 40) + 'px';
+
+
+    for (var i = 0; i < tooltips.length; i++) {
+      tooltips[i].style.top = y;
+      tooltips[i].style.left = x
+    }
+
+    for (var i = 0; i < tooltipsUp.length; i++) {
+      tooltipsUp[i].style.top = yp;
+      tooltipsUp[i].style.left = x;
+    }
+
+};
+
+
 
 function elementInViewport(myElement) {
 
@@ -34,10 +72,59 @@ window.addEventListener('scroll', function (event) {
 
     if(!audioDedicace.classList.contains('hidden')){
         audioDedicace.classList.add('hidden');
-	var audioStop = document.querySelector('.audioDedicace');
+        var audioStop = document.querySelector('.audioDedicace');
         audioStop.pause();
         audioStop.currentTime = 0;
     }
   }
+
+}, false);
+
+
+window.addEventListener('scroll', function (event) {
+
+  if(elementInViewport(pageChapitreOne)){
+    console.log('Element One is in the viewport!');
+
+    if(audioChapitreOne.classList.contains('hidden'))
+    {
+      audioChapitreOne.classList.remove('hidden');
+    }
+  }
+  else{
+    console.log('Element One is NOT in the viewport!');
+
+    if(!audioChapitreOne.classList.contains('hidden')){
+        audioChapitreOne.classList.add('hidden');
+        var audioStopOne = document.querySelector('.audioChapitre1');
+        audioStopOne.pause();
+        audioStopOne.currentTime = 0;
+    }
+  }
+
+
+}, false);
+
+window.addEventListener('scroll', function (event) {
+
+  if(elementInViewport(pageChapitreTwo)){
+    console.log('Element Two is in the viewport!');
+
+    if(audioChapitreTwo.classList.contains('hidden'))
+    {
+      audioChapitreTwo.classList.remove('hidden');
+    }
+  }
+  else{
+    console.log('Element Two is NOT in the viewport!');
+
+    if(!audioChapitreTwo.classList.contains('hidden')){
+        audioChapitreTwo.classList.add('hidden');
+        var audioStopTwo = document.querySelector('.audioChapitre2');
+        audioStopTwo.pause();
+        audioStopTwo.currentTime = 0;
+    }
+  }
+
 
 }, false);
---
+++ javascript/chanson.js
@@ -0,0 +1,59 @@
+const numberInput = document.querySelectorAll("input").length;
+let counter = 0;
+let correctAnswers = 0;
+let gradeH2 = document.querySelector(".note");
+
+function chansonWord(id){
+  const answer = document.getElementById(id);
+  const sibling = document.getElementById(id).nextSibling;
+
+  if((answer.value.toLowerCase())===id){
+    answer.classList.add("hidden");
+    sibling.classList.remove("hidden");
+    sibling.classList.add("correct");
+    counter += 1;
+    correctAnswers += 1;
+  }
+  else{
+    answer.classList.add("hidden");
+    sibling.classList.remove("hidden");
+    sibling.classList.add("incorrect")
+    counter += 1;
+  }
+
+  if(counter===numberInput){
+    let grade = (20/numberInput) * correctAnswers;
+    numberGrade = grade;
+    grade = grade.toString();
+
+    if(grade.length>2){
+      grade = grade.substr(0,5);
+    }
+
+    if(numberGrade <= 20 && numberGrade >=16){
+      gradeH2.textContent = "Note: " + grade + " Très Bien"
+    }
+    else if(numberGrade < 16 && numberGrade >=14)
+    {
+      gradeH2.textContent = "Note: " + grade + " Bien"
+    }
+    else if(numberGrade < 14 && numberGrade >=12)
+    {
+      gradeH2.textContent = "Note: " + grade + " Assez-Bien"
+    }
+    else if(numberGrade < 12 && numberGrade >=10)
+    {
+      gradeH2.textContent = "Note: " + grade + " Passable"
+    }
+    else if(numberGrade < 10 && numberGrade >=5)
+    {
+      gradeH2.textContent = "Note: " + grade + " Insuffisant"
+    }
+    else{
+      gradeH2.textContent = "Note: " + grade + " Très Insuffisant"
+    }
+
+
+  }
+
+}
